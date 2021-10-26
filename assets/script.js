b8c3ee53a1a9ca4af3db733e81070c9a42ac72f7
depotte6 <depotte6@gmail.com>
2021-10-26 06:52:22 -0400

    working on 5 day

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var APIKey = "09aa00c5fb117d6a47b132ca6f599e84";
+var APIKey = "ca2dc2e2fa9d729bac6bea4c5614b4ae";
 var weatherRootUrl = "https://api.openweathermap.org";
 var searchCityInput=$("#city");
 var searchHistory = [];
@@ -7,6 +7,7 @@ var searchCityInput = document.querySelector('#search-input');
 var todayDisplay = document.querySelector('#today');
 var futureDisplay = document.querySelector('#future');
 var searchHistoryDisplay= document.querySelector('#history');
+var fiveDay = document.querySelector('#future');
 
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
@@ -71,10 +72,32 @@ dayjs.extend(window.dayjs_plugin_timezone);
                 console.log(lat);
                 var lon = data.coord.lon;
                 console.log(lon);
-                fetchWeather(lat, lon);
+           
+                var apiUrl = `${weatherRootUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+              
+                fetch(apiUrl)
+            .then(function (res) {
+                console.log(res);
+                return res.json();
+            })
+                renderForecastCard(data);
+            
+            /*.then(function(data) {
+
+                console.log(data);
+                var newCard= $("<div>").attr("class", "card col-sm-2","background-color:rgb(60, 60, 109)");
+                fiveDay.append(newCard);
+                newCard.append($("<h4>").html(date++));
+                newCard.append($("<p>").html("Temp: " ));
+                newCard.append($("</p>").html("Wind: "));
+                newCard.append($("<p>").html("Humidity: "));
+            
+            })*/
             });
+
+            };
+            
         
-    };
 
 
 
@@ -113,20 +136,20 @@ dayjs.extend(window.dayjs_plugin_timezone);
 
     function renderForecastCard(data) {
         
-            var startDt = dayjs().tz(timezone).add(1, 'day').startOf('day').unix();
+            var startDt = dayjs().format('MM/DD/YYYY')+
             console.log(startDt);
-            var endDt = dayjs().tz(timezone).add(6, 'day').startOf('day').unix();
+            var endDt = startDt++;
             console.log(endDt);
             // variables for data from api
-            var unixTs = dailyForecast.dt;
+           // var unixTs = dailyForecast.dt;
             //var iconUrl = `https://openweathermap.org/img/w/${dailyForecast.weather.icon}.png`;
             //var iconDescription = dailyForecast.weather.description;
             
             
-            var dailyTemp = dailyForecast.temp;
+            var dailyTemp = data.temp;
            
-            var humidity  = dailyForecast.humidity;
-            var windMph = dailyForecast.wind_speed;
+            var humidity  = data.humidity;
+            var windMph = data.wind_speed;
         
             // Create elements for a card
             var col = document.createElement('div');
@@ -150,7 +173,7 @@ dayjs.extend(window.dayjs_plugin_timezone);
             humidityEl.setAttribute('class', 'card-text');
                 console.log(dailyTemp)
             // Add content to elements
-            cardTitle.textContent = dayjs.unix(unixTs).tz(timezone).format('M/D/YYYY');
+            //cardTitle.textContent = dayjs.unix(unixTs).tz(timezone).format('M/D/YYYY');
             //weatherIcon.setAttribute('src', iconUrl);
             //weatherIcon.setAttribute('alt', iconDescription);
             tempEl.textContent = `Temp: ${dailyTemp} °F`;
@@ -159,9 +182,9 @@ dayjs.extend(window.dayjs_plugin_timezone);
             futureDisplay.append(card);
             card.append(cardBody);
             cardBody.append(cardTitle, weatherIcon, tempEl, windEl, humidityEl);
-            for (var i = 0; i < dailyForecast.length; i++) {
-                if (dailyForecast[i].dt >= startDt && dailyForecast[i].dt < endDt) {
-                    renderForecastCard(dailyForecast[i], timezone);
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].dt >= startDt && data[i].dt < endDt) {
+                    renderForecastCard(data[i], timezone);
                   }
         
       
@@ -172,34 +195,27 @@ dayjs.extend(window.dayjs_plugin_timezone);
     }
       
 
-    function renderItems(city, data) {
-        //renderCurrent(city, data.current, data.timezone);
-        renderForecastCard(data.daily, data.timezone);
-    }
+    
+    function fetchCoords(search) {
+        var apiUrl = `${weatherApiRootUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherApiKey}`;
       
-    function fetchWeather(lat, lon) {
-        var apiUrl = `${weatherRootUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}`;
-              
         fetch(apiUrl)
-        .then(function (res) {
-            console.log(res);
+          .then(function (res) {
             return res.json();
-        })
-        .then(function (data) {
+          })
+          .then(function (data) {
             if (!data[0]) {
-                alert('Location not found');
+              alert('Location not found');
             } else {
-                appendToHistory(search);
-                renderForecastCard(data);
+              appendToHistory(search);
+              fetchWeather(data[0]);
             }
-        })
-        .catch(function (err) {
+          })
+          .catch(function (err) {
             console.error(err);
-        });
-    }
-    
-    
-
+          });
+      }
+      
     
               
    function handleSearchHistoryClick(e) {
@@ -253,26 +269,6 @@ searchHistoryDisplay.addEventListener('click', handleSearchHistoryClick);
         });
     };
 
-    function findFiveDay() {
-var queryURL1 = "http://api.openweathermap.org/data/2.5/onecall?q=" + search + "&units=imperial&appid=" + APIKey;
-fetch(queryURL1)
-.then(function(response) {
-    console.log(response);
-    return response.json() 
-    
-})
-.then(function(data) {
-
-    console.log(data);
-    var newCard= $("<div>").attr("class", "card col-sm-2","background-color:rgb(60, 60, 109)");
-    fiveDay.append(newCard);
-    newCard.append($("<h4>").html(date++));
-    newCard.append($("<p>").html("Temp: " ));
-    newCard.append($("</p>").html("Wind: "));
-    newCard.append($("<p>").html("Humidity: "));
-
-})
-}
 
 
 */
